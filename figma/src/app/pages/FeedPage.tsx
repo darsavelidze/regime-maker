0fd313104f38bb57a7ec67da67e6046605d5cde7
darsavelidze <darsavelli19@gmail.com>
2026-02-11 18:27:09 +0300

    chore: remove figma export files

--- figma/src/app/pages/FeedPage.tsx
+++
@@ -1,8 +0,0 @@
-export function FeedPage() {
-  return (
-    <div className="text-center py-12">
-      <h2 className="text-xl text-gray-600 mb-4">Лента</h2>
-      <p className="text-gray-500">Здесь будут отображаться публичные тренировки</p>
-    </div>
-  );
-}
